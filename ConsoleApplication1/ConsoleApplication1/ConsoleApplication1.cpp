27048f974f5baf9de44adcb33adc70e2f83c9df8
morozzik-2000 <nikmnv2000@gmail.com>
2025-06-22 16:48:22 +0300

    add raznost(func)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,10 +10,16 @@ void foo(int a) {
     cout << "Summa ravna = " << a << endl;
 }
 
+int raznost(int t, int y) {
+    return y - t;
+}
+
+
 int main()
 {
     int a = sum(4, 5);
     foo(a);
+    raznost(4, 10);
 }
 
 
