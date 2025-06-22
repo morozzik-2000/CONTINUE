66383f5ee5b57e8a28bcb9ab841ec0027d1ab903
morozzik-2000 <nikmnv2000@gmail.com>
2025-06-22 16:44:32 +0300

    add mulptiplex (func)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,11 +6,19 @@ int sum(int a, int b) {
     return a + b;
 }
 
+int multiplex(int c, int e) {
+    return c * e;
+}
 
 int main()
 {
     int a = sum(4, 5);
     cout << a;
+
+    int q = multiplex(4, 5);
+    cout << q;
+
+
 }
 
 
