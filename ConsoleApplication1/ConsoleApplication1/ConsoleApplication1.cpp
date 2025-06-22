cf4b55596c9a6f35c74f420e1b11314001298e4b
morozzik-2000 <nikmnv2000@gmail.com>
2025-06-22 16:46:18 +0300

    add foo (func)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,11 +6,14 @@ int sum(int a, int b) {
     return a + b;
 }
 
+void foo(int a) {
+    cout << "Summa ravna = " << a << endl;
+}
 
 int main()
 {
     int a = sum(4, 5);
-    cout << a;
+    foo(a);
 }
 
 
